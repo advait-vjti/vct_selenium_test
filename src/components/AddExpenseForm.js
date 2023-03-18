b71b086f8c07e35c06994e9b72b89eafbd633eb8
advait-vjti <advaitnaik7599@gmail.com>
2023-03-18 10:21:59 +0530

    Added Colour Category

--- src/components/AddExpenseForm.js
+++ src/components/AddExpenseForm.js
@@ -1,65 +1,90 @@
-import React, { useContext, useState } from 'react';
-import { AppContext } from '../context/AppContext';
-import { v4 as uuidv4 } from 'uuid';
+import React, { useContext, useState } from "react";
+import { AppContext, categories } from "../context/AppContext";
+import { v4 as uuidv4 } from "uuid";
 
 const AddExpenseForm = (props) => {
-	const { dispatch } = useContext(AppContext);
+  const { dispatch } = useContext(AppContext);
 
-	const [name, setName] = useState('');
-	const [cost, setCost] = useState('');
+  const [name, setName] = useState("");
+  const [cost, setCost] = useState("");
+  const [cat, setCat] = useState(Object.keys(categories)[0]);
+  
+  let cats = [];
 
-	const onSubmit = (event) => {
-		event.preventDefault();
-		const expense = {
-			id: uuidv4(),
-			name,
-			cost: parseInt(cost),
-		};
+  for (let c in categories) {
+    cats.push(categories[c]);
+  }
+  // console.log(cats);
 
-		dispatch({
-			type: 'ADD_EXPENSE',
-			payload: expense,
-		});
+  const onSubmit = (event) => {
+    event.preventDefault();
+    const expense = {
+      id: uuidv4(),
+      name,
+      cost: parseInt(cost),
+      category: categories[cat],
+    };
 
-		setName('');
-		setCost('');
-	};
+    dispatch({
+      type: "ADD_EXPENSE",
+      payload: expense,
+    });
 
-	return (
-		<form onSubmit={onSubmit}>
-			<div class='row'>
-				<div class='col-sm col-lg-4'>
-					<label for='name'>Name</label>
-					<input
-						required='required'
-						type='text'
-						class='form-control'
-						id='name'
-						value={name}
-						onChange={(event) => setName(event.target.value)}
-					/>
-				</div>
-				<div class='col-sm col-lg-4'>
-					<label for='cost'>Cost</label>
-					<input
-						required='required'
-						type='number'
-						class='form-control'
-						id='cost'
-						value={cost}
-						onChange={(event) => setCost(event.target.value)}
-					/>
-				</div>
-			</div>
-			<div class='row mt-3'>
-				<div class='col-sm'>
-					<button type='submit' class='btn btn-primary'>
-						Save
-					</button>
-				</div>
-			</div>
-		</form>
-	);
+    setName("");
+    setCost("");
+  };
+
+  return (
+    <form onSubmit={onSubmit}>
+      <div class="row">
+        <div class="col-sm col-lg-4">
+          <label for="name">Name</label>
+          <input
+            required="required"
+            type="text"
+            class="form-control"
+            id="name"
+            value={name}
+            onChange={(event) => setName(event.target.value)}
+          />
+        </div>
+        <div class="col-sm col-lg-4">
+          <label for="cost">Cost</label>
+          <input
+            required="required"
+            type="number"
+            class="form-control"
+            id="cost"
+            value={cost}
+            onChange={(event) => setCost(event.target.value)}
+          />
+        </div>
+        <div class="col-sm col-lg-4">
+          <label for="category">Category</label>
+          <select 
+            id="category" 
+            name="category" 
+            class="form-control"
+            onChange={(e) => {
+                // console.log(e.target.value);
+                setCat(e.target.value);
+            }} 
+            >
+            {cats.map((cat) => (
+              <option value={cat.name}>{cat.name}</option>
+            ))}
+          </select>
+        </div>
+      </div>
+      <div class="row mt-3">
+        <div class="col-sm">
+          <button type="submit" class="btn btn-primary">
+            Save
+          </button>
+        </div>
+      </div>
+    </form>
+  );
 };
 
 export default AddExpenseForm;
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -14,10 +14,16 @@ const ExpenseItem = (props) => {
 
 	return (
 		<li class='list-group-item d-flex justify-content-between align-items-center'>
-			{props.name}
 			<div>
-				<span class='badge badge-primary badge-pill mr-3' style={{color:'black', fontSize: 16, fontWeight: 'lighter'}}>£<span>{props.cost}</span></span>
-				<TiDelete size='1.5em' onClick={handleDeleteExpense} />
+				{props.name}
+				{/* <span style={{ marginLeft: 30,width: 30, border:'1px solid black', borderRadius: 2, backgroundColor: props.category.colour}}>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>	 */}
+			</div>	
+			<div style={{display: 'flex', justifyContent: 'space-between', width: 120}}>
+				<span style={{width: 30, border:'1px solid black', borderRadius: 2, backgroundColor: props.category.colour}}>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>	
+				<div>
+					<span class='badge badge-primary badge-pill mr-3' style={{color:'black', fontSize: 16, fontWeight: 'lighter'}}>£<span>{props.cost}</span></span>
+					<TiDelete size='1.5em' onClick={handleDeleteExpense} />
+				</div>	
 			</div>
 		</li>
 	);
